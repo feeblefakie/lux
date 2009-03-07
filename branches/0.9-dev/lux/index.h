b7d4874df10d2e985df420b99c95c4744823ce8d
hiroyu <hiroyu@8cf13c7a-5d4a-0410-81d5-811cc4f0bcaa>
2009-03-07 01:37:06 +0000

    removed 0.9-dev once

--- branches/0.9-dev/lux/index.h
+++
@@ -1,4 +0,0 @@
-#include "lux/luxconfig.h"
-#include "lux/api/indexer.h"
-#include "lux/document/document.h"
-#include "lux/api/document_config_parser.h"
--- branches/0.9-dev/lux/search.h
+++
@@ -1,5 +0,0 @@
-#include "lux/luxconfig.h"
-#include "lux/api/searcher.h"
-#include "lux/api/condition.h"
-#include "lux/api/document_config_parser.h"
-
